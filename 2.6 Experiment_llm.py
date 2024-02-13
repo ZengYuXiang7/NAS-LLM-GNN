dd0f2392a31c2885568249e853b084aec593ccc3
ZengYuXiang <397125932@qq.com>
2024-02-13 14:02:14 +0800

    把语言模型全部修正正确

--- 2.6 Experiment_llm.py
+++ 2.6 Experiment_llm.py
@@ -26,7 +26,7 @@ def run_in_py():
         RunExperiments(log, args)
 
     for exper in [8]:
-        for device_type in ['gpu']:
+        for device_type in ['dsp']:
             for dim in [128]:
                 for density in [0.1]:
                     for llm in [1]:
