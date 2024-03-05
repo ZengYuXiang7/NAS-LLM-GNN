2eda305b5a3ae943845d7eff29f7c58f8a30cdf2
ZengYuXiang <397125932@qq.com>
2024-03-05 15:00:32 +0800

    log上传

--- run_gru.py
+++ run_gru.py
@@ -138,44 +138,24 @@ class TensorDataset(torch.utils.data.Dataset):
         return self.indices.shape[0]
 
 
-# class LSTMModel(torch.nn.Module):
-#     def __init__(self, input_dim, hidden_dim, output_dim):
-#         super(LSTMModel, self).__init__()
-#         self.hidden_dim = hidden_dim
-#         self.transfer = torch.nn.Linear(6, hidden_dim)
-#         self.lstm = torch.nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=1, batch_first=True)
-#         self.fc = torch.nn.Linear(hidden_dim, output_dim)
-#
-#     def forward(self, x):
-#         one_hot_encoded = torch.nn.functional.one_hot(x.long(), num_classes=6).to(torch.float64)
-#         # print(one_hot_encoded.shape)
-#         x = self.transfer(one_hot_encoded)
-#         out, op_embeds = self.lstm(x)
-#         op_embeds = op_embeds.squeeze()
-#         out = self.fc(op_embeds)
-#         return out
-
-
 class LSTMModel(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(LSTMModel, self).__init__()
         self.hidden_dim = hidden_dim
-        self.transfer = torch.nn.Linear(input_dim, hidden_dim)
-        self.lstm = torch.nn.LSTM(hidden_dim, hidden_dim, num_layers=1, batch_first=True)
+        self.transfer = torch.nn.Linear(6, hidden_dim)
+        self.lstm = torch.nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=1, batch_first=True)
         self.fc = torch.nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         one_hot_encoded = torch.nn.functional.one_hot(x.long(), num_classes=6).to(torch.float64)
         # print(one_hot_encoded.shape)
         x = self.transfer(one_hot_encoded)
-        # x = self.transfer(x)
-        # LSTM returns output and a tuple of (hidden state, cell state)
-        # x = x.unsqueeze(0)
-        out, (hn, cn) = self.lstm(x)
-        # Squeeze to remove extra dimensions for single layer LSTM
-        hn = hn.squeeze()
-        out = self.fc(hn)
+        out, op_embeds = self.lstm(x)
+        op_embeds = op_embeds.squeeze()
+        out = self.fc(op_embeds)
         return out
+
+
 
 class Model(torch.nn.Module):
     def __init__(self, args):
@@ -347,7 +327,7 @@ if __name__ == '__main__':
     parser.add_argument('--rounds', type=int, default=5)
 
     parser.add_argument('--dataset', type=str, default='cpu')  #
-    parser.add_argument('--model', type=str, default='LSTM')  #
+    parser.add_argument('--model', type=str, default='GRU')  #
 
     # Experiment
     parser.add_argument('--density', type=float, default=0.05)
