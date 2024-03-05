dacecfb0748216337ce0d95de693578dfe26a7ee
ZengYuXiang <397125932@qq.com>
2024-03-05 15:00:32 +0800

    Baselines_LSTM上传

---
+++ 3.5 Run_baselines3.sh
@@ -0,0 +1,12 @@
+python run_lstm.py --rounds 5 --density 0.01 --model GRU --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.02 --model GRU --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.03 --model GRU --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.04 --model GRU --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.05 --model GRU --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+   
+python run_lstm.py --rounds 5 --density 0.01 --model GRU --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.02 --model GRU --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.03 --model GRU --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.04 --model GRU --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+python run_lstm.py --rounds 5 --density 0.05 --model GRU --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 32 --experiment 1 --record 1
+   
