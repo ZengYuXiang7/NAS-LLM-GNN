39a9bb27982503f76d4b3275f0d4ffb91e55f4ff
yxzeng <397125932@qq.com>
2024-03-17 23:49:13 +0800

    log

--- 3.9 Run_hyper.sh
+++ 3.9 Run_hyper.sh
@@ -1,16 +1,16 @@
-python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 10 --experiment 1 --record 1
+#python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 10 --experiment 1 --record 1
+#
+#python run_llm_gcn.py --rounds 5 --density 0.05 --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 10 --experiment 1 --record 1
+#
+#python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 20 --experiment 1 --record 1
+#
+#python run_llm_gcn.py --rounds 5 --density 0.05 --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 20 --experiment 1 --record 1
+#
+#python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 30 --experiment 1 --record 1
+#
+#python run_llm_gcn.py --rounds 5 --density 0.05 --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 30 --experiment 1 --record 1
    
-python run_llm_gcn.py --rounds 5 --density 0.05 --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 10 --experiment 1 --record 1
-   
-python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 20 --experiment 1 --record 1
-   
-python run_llm_gcn.py --rounds 5 --density 0.05 --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 20 --experiment 1 --record 1
-   
-python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 30 --experiment 1 --record 1
-   
-python run_llm_gcn.py --rounds 5 --density 0.05 --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 30 --experiment 1 --record 1
-   
-python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 40 --experiment 1 --record 1
+#python run_llm_gcn.py --rounds 5 --density 0.05 --dataset cpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 40 --experiment 1 --record 1
    
 python run_llm_gcn.py --rounds 5 --density 0.05 --dataset gpu --epochs 300 --bs 1 --lr 0.0004 --decay 0.0005 --program_test 1 --dimension 40 --experiment 1 --record 1
    
